03346088790270956e78453c784fdb9f489e8ecb
Siddhesh-repo <potesiddhesh20@gmail.com>
2025-07-18 22:24:58 +0530

    Initial commit

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Products
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -8,4 +9,14 @@ def index(request):
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         product_objects = product_objects.filter(title__icontains=item_name)
-    return render(request,'kart/index.html',{'product_objects':product_objects}) 
+    
+    paginator = Paginator(product_objects,3)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
+    return render(request,'kart/index.html',{'product_objects':product_objects})  
+
+
+def detail(request,id):
+    product_object = Products.objects.get(id=id)
+    return render(request,'kart/detail.html',{'product_object':product_object})
